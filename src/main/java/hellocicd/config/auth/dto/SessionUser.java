5a71c764f3712d1ebcfd61e8c80954ad95eaa3a0
yonghyun123 <model9960@naver.com>
2023-02-12 20:13:46 +0900

    OAuth2 셋팅 및 Adapter 설정 전

---
+++ src/main/java/hellocicd/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package hellocicd.config.auth.dto;
+
+import hellocicd.domain.user.User;
+
+import java.io.Serializable;
+
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
